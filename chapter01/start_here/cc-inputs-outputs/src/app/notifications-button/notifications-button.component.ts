e6060d0f30096cc133c7651d8cf674d174cdad27
DFVAZ <89922865+DFVAZ@users.noreply.github.com>
2023-03-23 09:30:12 +0000

    ex1 solved

--- chapter01/start_here/cc-inputs-outputs/src/app/notifications-button/notifications-button.component.ts
+++ chapter01/start_here/cc-inputs-outputs/src/app/notifications-button/notifications-button.component.ts
@@ -1,3 +1,4 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-button.component.scss']
 })
 export class NotificationsButtonComponent implements OnInit {
-
+  @Input() count =0
   constructor() { }
 
   ngOnInit(): void {
